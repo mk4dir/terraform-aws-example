de5241eae5441e8e2cd961642825da58c18078dd
Kadir <kadir.karakoc@vngrs.com>
2021-06-16 17:02:36 +0300

    security group ingress updated

--- main.tf
+++ main.tf
@@ -94,13 +94,13 @@ resource "aws_security_group" "allow-ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress = [ {
+  ingress = {
     description = "value"
     from_port = 8080
     to_port = 8080
     protocol = "tcp"
     cidr_blocks = [ "0.0.0.0/0" ]
-  } ]
+  }
 
   egress {
     from_port   = 0
